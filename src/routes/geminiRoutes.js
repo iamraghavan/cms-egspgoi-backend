ac58a6862f5448b208d1b103b5e273b922123a7f
Raghavan <developer.raghavan@gmail.com>
2025-12-15 21:10:49 +0530

    feat: add Gemini API routes for blog generation

--- src/routes/geminiRoutes.js
+++ src/routes/geminiRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { generateBlog } = require('../controllers/geminiController');
-const { authenticate } = require('../middleware/authMiddleware');
 
-router.post('/generate', authenticate, generateBlog);
+router.post('/generate', generateBlog);
 
 module.exports = router;
