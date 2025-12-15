719ca7100964179afe20e6fd1a660f3091b23512
Raghavan <developer.raghavan@gmail.com>
2025-12-15 21:21:09 +0530

    10210

--- src/routes/geminiRoutes.js
+++ src/routes/geminiRoutes.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { generateBlog } = require('../controllers/geminiController');
 
-router.post('/generate', generateBlog);
+// Simple API Key Middleware
+const checkApiKey = (req, res, next) => {
+    const { key } = req.query;
+    if (key === '232003') {
+        return next();
+    }
+    return res.status(401).json({ message: 'Unauthorized: Invalid API Key found' });
+};
+
+router.post('/generate', checkApiKey, generateBlog);
 
 module.exports = router;
