2ad612b6210a7c4b58d9ea426b542886b0e7fabe
iamraghavan <developer.raghavan@gmail.com>
2026-01-15 23:38:17 +0530

    0210

--- src/routes/smartfloApiRoutes.js
+++ src/routes/smartfloApiRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { hangupCall, getCallRecords, getActiveCall, getLiveCalls, clickToCall } = require('../controllers/smartfloController');
 const { checkPermission } = require('../middleware/rbacMiddleware');
+const { authenticate } = require('../middleware/authMiddleware');
 
 // Hangup a call
 router.post('/call/hangup', hangupCall);
@@ -10,12 +11,12 @@ router.post('/call/hangup', hangupCall);
 router.get('/call/records', getCallRecords);
 
 // Get All Live Calls (Super Admin)
-router.get('/live-calls', getLiveCalls);
+router.get('/live-calls', authenticate, getLiveCalls);
 
 // Get Active Call Status (Backend Polling)
-router.get('/active-call/:lead_id', getActiveCall);
+router.get('/active-call/:lead_id', authenticate, getActiveCall);
 
 // Click to Call (Admission Executive and above)
-router.post('/click-to-call', checkPermission('click_to_call'), clickToCall);
+router.post('/click-to-call', authenticate, checkPermission('click_to_call'), clickToCall);
 
 module.exports = router;
